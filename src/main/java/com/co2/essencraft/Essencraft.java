0bafb9d1832768a0775f5e25b677d2cc5f148a84
defiant810 <defiant810@gmail.com>
2014-01-08 19:12:49 -0700

    Initial commit
    And we're off!

---
+++ src/main/java/com/co2/essencraft/Essencraft.java
@@ -0,0 +1,37 @@
+package com.co2.essencraft;
+
+import com.co2.essencraft.lib.Reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+public class Essencraft 
+{
+	@Instance(value = Reference.MOD_ID)
+	public static Essencraft instance;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+}
---
+++ src/main/java/com/co2/essencraft/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.co2.essencraft.lib;
+
+public class Reference 
+{
+	public static final String MOD_ID = "essencraft"; //NEVER EVER EVER CHANGE
+	public static final String MOD_NAME = "Essencraft";
+	public static final String MOD_VERSION = "0.0.1";
+}
