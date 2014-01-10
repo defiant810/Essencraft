058d99c7ed8ba142eb34fab82cc49e7e755dfb7c
defiant810 <defiant810@gmail.com>
2014-01-09 19:48:18 -0700

    Finally fixed merging fiasco.

--- src/main/java/com/co2/essencraft/item/ItemCoconut.java
+++ src/main/java/com/co2/essencraft/item/ItemCoconut.java
@@ -2,13 +2,8 @@ package com.co2.essencraft.item;
 
 import com.co2.essencraft.lib.StringLib;
 
-<<<<<<< HEAD
 public class ItemCoconut extends ItemFoodESC
 {
-=======
-public class ItemCoconut extends ItemFoodESC{
-
->>>>>>> 901e7d5e27ba4385ffa08d7ebf776a1e73d58d18
 	public ItemCoconut(int id)
 	{
 		super(id, 2, false);
