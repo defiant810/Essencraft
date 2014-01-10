261291ee0efbcee68d2a4be9f79c45e90d8c760c
imdodrill <imdodrill100@gmail.com>
2014-01-09 17:39:16 -0800

    added coconut to ItemESC

--- src/main/java/com/co2/essencraft/item/ModItems.java
+++ src/main/java/com/co2/essencraft/item/ModItems.java
@@ -8,10 +8,12 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class ModItems 
 {
 	public static ItemESC rawSaltPile;
+	public static ItemESC coconut;
 	
 	public static void init()
 	{
 		 rawSaltPile = new ItemRawSaltPile(ItemIds.RAW_SALT_PILE);
+		 coconut = new ItemCoconut(ItemIds.COCONUT);
 		 
 		 GameRegistry.registerItem(rawSaltPile, "item." + StringLib.RAW_SALT_PILE_NAME);
 	}
