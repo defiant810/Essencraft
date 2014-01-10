15a415f3e210ba06afede94186153e30a02a5c88
imdodrill <imdodrill100@gmail.com>
2014-01-09 17:18:04 -0800

    fixed coconut item

--- src/main/java/com/co2/essencraft/item/ItemCoconut.java
+++ src/main/java/com/co2/essencraft/item/ItemCoconut.java
@@ -4,7 +4,7 @@ import com.co2.essencraft.lib.StringLib;
 
 public class ItemCoconut extends ItemESC{
 
-	public ItemRawSaltPile(int id)
+	public ItemCoconut(int id)
 	{
 		super(id);
 		this.setUnlocalizedName(StringLib.COCONUT_NAME);
