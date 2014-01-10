3b8add0a4ddde01d04ecfce046e8fe2549b9110e
defiant810 <defiant810@gmail.com>
2014-01-09 19:43:06 -0700

    Fixed merge issues

--- src/main/java/com/co2/essencraft/item/ItemCoconut.java
+++ src/main/java/com/co2/essencraft/item/ItemCoconut.java
@@ -2,11 +2,16 @@ package com.co2.essencraft.item;
 
 import com.co2.essencraft.lib.StringLib;
 
+<<<<<<< HEAD
 public class ItemCoconut extends ItemESC
 {
+=======
+public class ItemCoconut extends ItemFoodESC{
+
+>>>>>>> 901e7d5e27ba4385ffa08d7ebf776a1e73d58d18
 	public ItemCoconut(int id)
 	{
-		super(id);
+		super(id, 2, false);
 		this.setUnlocalizedName(StringLib.COCONUT_NAME);
 	}
 }
