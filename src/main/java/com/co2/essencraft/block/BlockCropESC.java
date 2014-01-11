60c259caceb72d41827a15dafcc7f65ceaed3618
defiant810 <defiant810@gmail.com>
2014-01-11 14:00:58 -0700

    Got Rye working, no longer need child classes
    All crops can now be instances of BlockCropESC, with various info passed
    to the constructor. Rye now works completely.

--- src/main/java/com/co2/essencraft/block/BlockCropESC.java
+++ src/main/java/com/co2/essencraft/block/BlockCropESC.java
@@ -1,38 +1,52 @@
 package com.co2.essencraft.block;
 
+import java.util.ArrayList;
 import java.util.Random;
+
+import com.co2.essencraft.lib.StringLib;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockFlower;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeDirection;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class BlockCropESC extends BlockFlower{
-
-	@SideOnly(Side.CLIENT)
-    public Icon[] iconArray;
-	private float growthRate;
+public class BlockCropESC extends BlockFlower
+{
+	protected static final int GROWTH_STAGES = 8;
 	
-	public BlockCropESC(int id, float growthRate)
+	@SideOnly(Side.CLIENT)
+    public Icon[] icons;
+	
+	protected final float growthRate;
+	protected final int seedId; //Id of the seed dropped
+	protected final int seedMeta; //Metadata of the seed dropped
+	protected final int dropId; //Id of the grain item dropped
+	protected final int dropMeta; //Meta of the grain item dropped
+	protected int dropAmt = 1; //The amount of grain item dropped
+	
+	public BlockCropESC(int id, float growthRate, int seedId, int seedMeta, int dropId, int dropMeta)
 	{
 		super(id);
 		this.growthRate = growthRate;
+		this.seedId = seedId;
+		this.seedMeta = seedMeta;
+		this.dropId = dropId;
+		this.dropMeta = dropMeta;
 		setTickRandomly(true);
 		setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
 	}
 	
-	/**
-     * Gets passed in the blockID of the block below and supposed to return true if its allowed to grow on the type of
-     * blockID passed in. Args: blockID
-     */
 	protected boolean canThisPlantGrowThisBlockID(int par1)
 	{
 		return par1 == Block.tilledField.blockID;
 	}
 	
+	@Override
 	public void updateTick(World par1World, int par2, int par3, int par4, Random par5Random)
 	{
 		super.updateTick(par1World, par2, par3, par4, par5Random);
@@ -60,7 +74,7 @@ public class BlockCropESC extends BlockFlower{
      */
 	private float getGrowthRate(World par1World, int par2, int par3, int par4)
 	{
-		float f = growthRate;
+		float f = 1.0f;
         int l = par1World.getBlockId(par2, par3, par4 - 1);
         int i1 = par1World.getBlockId(par2, par3, par4 + 1);
         int j1 = par1World.getBlockId(par2 - 1, par3, par4);
@@ -104,6 +118,56 @@ public class BlockCropESC extends BlockFlower{
             f /= 2.0F;
         }
 
-        return f;
+        return f * this.growthRate;
+	}
+	
+	@Override 
+    public ArrayList<ItemStack> getBlockDropped(World world, int x, int y, int z, int metadata, int fortune)
+    {
+        ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+        
+        //Force at least one seed to drop
+        ret.add(new ItemStack(this.seedId, 1, this.seedMeta));
+
+        //If the plant is fully grown
+        if (metadata >= 7)
+        {
+        	//Force it to drop a grain item
+        	ret.add(new ItemStack(this.dropId, this.dropAmt, this.dropMeta));
+            for (int n = 0; n < 3 + fortune; n++)
+            {
+                if (world.rand.nextInt(15) <= metadata)
+                {
+                    ret.add(new ItemStack(this.seedId, 1, this.seedMeta));
+                }
+            }
+        }
+
+        return ret;
+    }
+	
+	@Override
+	public int getRenderType()
+	{
+		return 6;
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public Icon getIcon(int side, int meta)
+	{
+		if(meta < 0 || meta > 7)
+			meta = 7;
+		
+		return icons[meta];
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister iconRegister)
+	{
+		icons = new Icon[GROWTH_STAGES];
+		for (int i = 0; i < icons.length; i++)
+			icons[i] = iconRegister.registerIcon(StringLib.ASSET_PREFIX + "block" + this.textureName + "stage0" + i);
 	}
 }
--- src/main/java/com/co2/essencraft/block/ModBlocks.java
+++ src/main/java/com/co2/essencraft/block/ModBlocks.java
@@ -1,18 +1,24 @@
 package com.co2.essencraft.block;
 
+import net.minecraft.item.Item;
+
 import com.co2.essencraft.lib.BlockIds;
+import com.co2.essencraft.lib.ItemIds;
 import com.co2.essencraft.lib.StringLib;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 public class ModBlocks 
 {
-	public static BlockRyeCrop ryeCrop;
+	public static BlockCropESC ryeCrop;
 	
 	public static void init()
 	{
-		ryeCrop = new BlockRyeCrop(BlockIds.RYE_CROP, 1.0f);
+		ryeCrop = (BlockCropESC) new BlockCropESC(BlockIds.RYE_CROP, 1.0f, ItemIds.SEED, 4/*rye seed meta*/, Item.wheat.itemID/*TODO: Grain id*/, 0/*TODO: rye metadata*/).setTextureName("GrainRye");
 		
 		GameRegistry.registerBlock(ryeCrop, "tile." + StringLib.RYE_CROP_NAME);
+		
+		LanguageRegistry.addName(ryeCrop, "Rye Crop");
 	}
 }
