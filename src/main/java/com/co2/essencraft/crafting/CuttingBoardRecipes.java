04e35fce2aef0c7de7d215dd03e43df456a92199
imdodrill <imdodrill100@gmail.com>
2014-01-20 15:01:07 -0800

    fixed error in recipe

--- src/main/java/com/co2/essencraft/crafting/CuttingBoardRecipes.java
+++ src/main/java/com/co2/essencraft/crafting/CuttingBoardRecipes.java
@@ -22,7 +22,7 @@ public class CuttingBoardRecipes
 		for(int i = 0; i < StringLib.CUT_VEGETABLE_NAMES.length; i++)
 		{
 			if(StringLib.CUT_VEGETABLE_NAMES[i] != null)
-				CBCraftingManager.addRecipe(new ItemStack(ItemIds.FRUIT, 1, i), KnifeType.CHEF, new ItemStack(ItemIds.CUT_FRUIT, 1, i));
+				CBCraftingManager.addRecipe(new ItemStack(ItemIds.VEGETABLE, 1, i), KnifeType.CHEF, new ItemStack(ItemIds.CUT_VEGETABLE, 1, i));
 		}
 	}
 }
